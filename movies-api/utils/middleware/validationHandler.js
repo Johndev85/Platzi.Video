be352e3e2500b51183ed6f7a612c90991d8dfb88
John Botero <jhondark@gmail.com>
2020-05-08 14:20:59 -0500

    implementando middleware boom

---
+++ movies-api/utils/middleware/validationHandler.js
@@ -0,0 +1,16 @@
+const boom = require('@hapi/boom')
+
+function validate(params) {
+    return false
+}
+
+function validationHandler(schema, check = "body") {
+    return function (req, res, next) {
+        const error = validate(req[check], schema)
+
+        error ? next(boom.badRequest(error)) : next()
+    }
+}
+
+module.exports = validationHandler
+
