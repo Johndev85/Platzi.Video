1ff04bdf6a81058b6db6e3b0437935843c1ed2f2
John Botero <jhondark@gmail.com>
2020-05-18 17:24:19 -0500

    integración webpack con express, compiled sucessfully

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,29 @@
 import express from 'express';
+import dotenv from 'dotenv'
+import webpack from 'webpack'
 
+dotenv.config();
+
+const { ENV, PORT } = process.env;
 const app = express();
 
+if (ENV === 'development') {
+    console.log('Development config');
+    const webpackConfig = require('../../webpack.config')
+    const webpackDevMiddleware = require('webpack-dev-middleware')
+    const webpackHotMiddleware = require('webpack-hot-middleware')
+    const compiler = webpack(webpackConfig)
+    const serverConfig = { port: PORT, hot: true }
+
+    app.use(webpackDevMiddleware(compiler, serverConfig))
+    app.use(webpackHotMiddleware(compiler))
+}
+
 app.get('*', (req, res) => {
-    res.send({hello: 'express activado'})
+    res.send({hello: 'express activado con nodemon'})
 });
 
-app.listen(3000, (err) => {
+app.listen(PORT, (err) => {
     if (err) console.log(err);
     else console.log('Server running on port 3000');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
+
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/frontend/index.js', 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true'],
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -56,6 +59,7 @@ module.exports = {
     historyApiFallback: true,
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebPackPlugin({
       template: './public/index.html',
       filename: './index.html',
