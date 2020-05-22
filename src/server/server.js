c6c157ed5df5cdc75b26f32d30050d09ac23625a
John Botero <jhondark@gmail.com>
2020-05-21 19:46:10 -0500

    Se agrega hash y manifest

--- src/server/server.js
+++ src/server/server.js
@@ -11,7 +11,7 @@ import { StaticRouter } from 'react-router-dom'
 import serverRoutes from '../frontend/routes/serverRoutes'
 import reducer from '../frontend/reducers';
 import initialState from '../frontend/initialState'
-
+import getManifest from './getManifest'
 
 dotenv.config();
 
@@ -29,18 +29,25 @@ if (ENV === 'development') {
     app.use(webpackDevMiddleware(compiler, serverConfig))
     app.use(webpackHotMiddleware(compiler))
 } else {
+    app.use((req, res, next) => {
+        if (!req.hashManifest) req.hashManifest = getManifest()
+            next()
+    });
     app.use(express.static(`${__dirname}/public`))
     app.use(helmet())
     app.use(helmet.permittedCrossDomainPolicies())
     app.disable('x-powered-by')
 }
 
-const setResponse = (html, preloadedState) => {
+const setResponse = (html, preloadedState, manifest) => {
+    const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css'
+    const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js'
+
     return (
         `<!DOCTYPE html>
         <html lang="en">
         <head>
-            <link rel="stylesheet" href="assets/app.css" type="text/css">
+            <link rel="stylesheet" href="${mainStyles}" type="text/css">
             <title>Platzi Video</title>
         </head>
         <body>
@@ -53,7 +60,7 @@ const setResponse = (html, preloadedState) => {
                 '\\u003c'
               )}
               </script>
-            <script src="assets/app.js" type="text/javascript"></script>
+            <script src="${mainBuild}" type="text/javascript"></script>
         </body>
     </html>`
     )
@@ -69,7 +76,7 @@ const renderApp = (req, res) => {
             </StaticRouter>
         </Provider>
     )
-    res.send(setResponse(html, preloadedState))
+    res.send(setResponse(html, preloadedState, req.hashManifest))
 }
 
 app.get('*', renderApp)
